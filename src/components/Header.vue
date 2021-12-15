aaffd8d26990388b977e959910d14e12663f65fa
Cleonart <zredhard@gmail.com>
2021-12-16 01:53:20 +0900

    Add logout feature

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,19 +7,35 @@
     "
     class="py-3"
   >
+    <v-snackbar v-model="dialog">
+      {{ dialog_txt }}
+    </v-snackbar>
+
     <img @click="gotoHome" src="@/assets/pic.png" style="width: 120px" />
 
-    <v-avatar
-      style="
-        font-family: 'Sans-Serif';
-        position: absolute;
-        left: 15px;
-        top: 12px;
-      "
-      color="primary"
-      size="32"
-      ><img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"
-    /></v-avatar>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-avatar
+          v-bind="attrs"
+          v-on="on"
+          style="
+            font-family: 'Sans-Serif';
+            position: absolute;
+            left: 15px;
+            top: 12px;
+          "
+          color="primary"
+          size="32"
+        >
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+        </v-avatar>
+      </template>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title @click="logOut()">Keluar</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
     <!-- Cart Badge [Top Right] -->
     <v-badge
@@ -36,12 +52,27 @@
 
 <script>
 export default {
+  data() {
+    return {
+      dialog: false,
+      dialog_txt: true,
+    };
+  },
   methods: {
     gotoHome() {
       this.$router.push("/");
     },
     gotoShoppingCart() {
       this.$router.push("/shoppingcart");
+    },
+    logOut() {
+      let app = this;
+      this.dialog = true;
+      this.dialog_txt = "Anda berhasil keluar dari akun anda";
+      sessionStorage.clear();
+      setTimeout(function () {
+        app.$router.replace("/login");
+      }, 1000);
     },
   },
 };
